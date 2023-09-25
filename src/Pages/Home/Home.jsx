a85b301b5040e797e537c7f10ea70e6539938451
safinxr <safinxr@gmail.com>
2023-09-26 00:33:12 +0600

    home card part done and responsive done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return(
         <div>
             <Banner></Banner>
-            <main className="max-w-7xl mx-auto my-44 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <main className="max-w-7xl mx-auto my-44 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 px-3 md:px-7 lg:px-0">
                 {
                     allData.map(data => <HomeCards data={data} key={data.id}></HomeCards>)
                 }
--- src/Pages/Home/HomeCards.jsx
+++ src/Pages/Home/HomeCards.jsx
@@ -1,12 +1,31 @@
+
+
 export default function HomeCards({ data }) {
-    const { id, title, category, description, Picture, category_bg_color, text_btn_color, card_bg_color } = data
-    console.log(text_btn_color);
+    const {id, title, category, Picture, category_bg_color, text_btn_color, card_bg_color } = data
+    
+    const cardClick =(ids)=>{
+        console.log(ids);
+    }
+
+
+
     return (
-        <div className={`bg-[${card_bg_color}]`}>
-            <img src={Picture} alt="" />
+        <div 
+        onClick={()=>cardClick(id)}
+        style={{backgroundColor:card_bg_color}} 
+        className="lg:h-[323px] rounded-lg shadow-lg duration-300 active:scale-[0.96]">
+            <img className="rounded-t-lg" src={Picture} alt="" />
             <div className="p-4">
-                <p className= {`bg-[${category_bg_color}] text-[${text_btn_color}] text-sm px-2 inline-block py-0.5 rounded font-medium`}>{category}</p>
-                <h3 className={`text-[${text_btn_color}] mt-2 text-xl font-bold`}>{title}</h3>
+                <p 
+                style={{backgroundColor:category_bg_color, color:text_btn_color}} className="text-sm px-2 inline-block py-0.5 rounded font-medium">
+                    {category}
+                </p>
+
+                <h3 
+                style={{color:text_btn_color}}
+                className="mt-2 text-xl font-bold">
+                    {title}
+                </h3>
             </div>
 
         </div>
