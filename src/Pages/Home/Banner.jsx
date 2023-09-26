cd0090431cf0bad07da5bec8bf16ac78b4bc3326
safinxr <safinxr@gmail.com>
2023-09-26 18:36:27 +0600

    earch function done

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
-export default function Banner() {
+export default function Banner({searchFun}) {
     const [inputValue, setInputValue] = useState('');
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
+    const searchClick = ()=>{
+        searchFun(inputValue);
+    }
+    
     return (
         <div className="h-[400px] lg:h-[600px] flex flex-col justify-center items-center mt-[-98px] bg-[url('https://i.ibb.co/nQKCNtf/banner-bg.jpg')] bg-fixed bg-repeat bg-white bg-blend-overlay bg-opacity-90">
             <h1 className="text-xl md:text-3xl lg:text-5xl font-bold mb-12">I Grow By Helping People In Need</h1>
@@ -14,7 +18,10 @@ export default function Banner() {
                      type="text" 
                      placeholder="Search here...." />
 
-                    <button className="bg-[#FF444A] text-white px-5 py-1.5 md:py-3 rounded-r-lg ms-[-6px]">Search</button>
+                    <button 
+                    onClick={searchClick}
+                    className="bg-[#FF444A] text-white px-5 py-1.5 md:py-3 rounded-r-lg ms-[-6px]"
+                    >Search</button>
                 </div>
         </div>
     )
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,27 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner";
 import HomeCards from "./HomeCards";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [finalData, setFinalData] = useState([])
     const allData = useLoaderData();
+    const searchFun =(key)=>{
+        const newKey = key.toLowerCase();
+        const newData = allData.filter(data => data.category.toLowerCase().includes(newKey))
+        setFinalData(newData);
+    }
+    useEffect(()=>{
+        searchFun('')
+    },[])
+
+    
     return(
         <div>
-            <Banner></Banner>
+            <Banner searchFun={searchFun}></Banner>
             <main className="max-w-7xl mx-auto my-6 md:my-20 lg:my-40 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 px-3 md:px-7 lg:px-0">
                 {
-                    allData.map(data => <HomeCards data={data} key={data.id}></HomeCards>)
+                    finalData.map(data => <HomeCards data={data} key={data.id}></HomeCards>)
                 }
             </main>
         </div>
